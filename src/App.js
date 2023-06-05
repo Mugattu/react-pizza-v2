5fe19fc3ed64cee512100bdddd8979641838f9a5
malik <gaspar-2021@mail.ru>
2023-06-05 18:11:23 +0300

    letter

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
             <Categories />
             <Sort />
           </div>
-          <h2 className="content__title">Все пиццы</h2>
+          <h2 className="content__title">Все пиццыs</h2>
           <div className="content__items">
             {pizzas.map((obj) => (
               <PizzaBlock {...obj} />
